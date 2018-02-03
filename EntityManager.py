7a328921c50c7a9950f0750aafc02e5817954581
Oleksii <kalimdor07@gmail.com>
2018-02-03 17:13:21 +0100

    deleted

--- EntityManager.py
+++ EntityManager.py
@@ -1,14 +1,15 @@
 import  requests
 
-
 url = 'https://api.wit.ai/entities?v=20170307'
 
-auth_tok = 'KT4AR7CGRPLN47WP25YXMRJJ6IU6SNE2'
+auth_tok = 'XEXPZR2CLQQIXLS7ZS4CMWUNZM7NIERE'
 
 hed = {'Authorization': 'Bearer ' + auth_tok}
+
 
 def getData(value, expressions) :
     return {"doc": value+"Doc", "lookups": ["trait"], "id": (value+"Id"), "values": [{"value": value, "expressions": expressions}]}
 
+
 def addEntity(value, epxresstions) :
-    return requests.post(url = url, json = getData(value, epxresstions), headers=hed )
+    return requests.post(url = url, json = getData(value, epxresstions), headers=hed)
