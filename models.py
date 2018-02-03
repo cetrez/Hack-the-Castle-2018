9723b223a7484dd212df78426a89b0f8131dae39
Tulga <sw06d103@gmail.com>
2018-02-03 12:51:55 +0100

    Model fixes

--- models.py
+++ models.py
@@ -13,14 +13,10 @@ class Group(db.Entity):
 class Participant(db.Entity):
     name = Required(str)
     fb_id = Required(str)
-    group = Required(Group)
+    group = Optional(Group)
 
 class Question(db.Entity):
     question = Required(str)
-    template = Required('Questionaire')
-
-class Questionaire(db.Entity):
-    questions = Set(Question)
 
 class Feedback:
     question = Required(Question)
@@ -29,4 +25,31 @@ class Feedback:
 
 db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
+
+@db_session
+def create_participant(name, fb_id):
+    p = Participant(name=name, fb_id=fb_id)
+    return p
+
+@db_session
+def select_participants():
+    return select(p for p in Participant)[:]
+
+@db_session
+def select_questions():
+    return select(p for p in Question)[:]
+
+@db_session
+def create_question(q):
+    p = Question(question=q)
+    return p
+
+@db_session
+def select_feedbacks():
+    return select(p for p in Feedback)[:]
+
+@db_session
+def create_question(q, p, a):
+    f = Feedback(question=q, participant=p, answer=a)
+    return f
 
