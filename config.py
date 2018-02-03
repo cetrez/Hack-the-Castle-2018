67b2ef4e8513be82b9489fd76262deb60f20256a
Oleksii Prykhodko <kalimdor07@gmail.com>
2018-02-03 14:48:54 +0000

    base

---
+++ config.py
@@ -0,0 +1,5 @@
+CONFIG = {
+    'FACEBOOK_TOKEN': 'EAAEg9PWjtr8BAKzKdZCdrbmk1muVrVmZAjwqoZBKkzYsu7epGFtvZBTgCpQYJ8flGSxT146wpG6nIlZCaYDaw3kUdn17lVjH4tkMrSioNUiOJdOwZBZAh8JZCsIb3Jq3Awx0t7Q2RQM5jabgDHb6MK8mxoPJLcCcKTDrRWfzA2LT9gZDZD',
+    'VERIFY_TOKEN': '12345678901',
+    'SERVER_URL': 'https://dierme.pythonanywhere.com/'
+}
