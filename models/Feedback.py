81caddacc40cf6cf45c3a87e9599125a5905617b
Oleksii <kalimdor07@gmail.com>
2018-02-10 12:57:54 +0100

    relations updated

--- models/Feedback.py
+++ models/Feedback.py
@@ -1,12 +1,14 @@
 from pony.orm import *
 from models.DataBase import DataBase
+from models.Participant import Participant
+from models.Question import Question
 
 db = DataBase.get_database()
 
 
 class Feedback(db.Entity):
     id = PrimaryKey(int, auto=True)
-    participant = Required(str)
+    participant = Required('Participant')
     question = Required('Question')
     answer = Required(str)
 
@@ -18,6 +20,8 @@ class Feedback(db.Entity):
 
     @staticmethod
     @db_session
-    def create_feedback(self, q, p, a):
+    def create_feedback(q_id, p_id, a):
+        q = Question[q_id]
+        p = Participant[p_id]
         f = Feedback(question=q, participant=p, answer=a)
         return f
