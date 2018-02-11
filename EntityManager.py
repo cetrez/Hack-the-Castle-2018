fc9739d6f7cb92c3087ca9be8f5066f58950b0cd
Oleksii <kalimdor07@gmail.com>
2018-02-11 15:46:38 +0100

    token moved to config

--- EntityManager.py
+++ EntityManager.py
@@ -1,14 +1,18 @@
-import  requests
+import requests
+from config import CONFIG
 
-url = 'https://api.wit.ai/entities?v=20160526'
+url = 'https://api.wit.ai/apps?v=20170307'
 
-auth_tok = 'KT4AR7CGRPLN47WP25YXMRJJ6IU6SNE2'
-
-hed = {'Authorization': 'Bearer ' + auth_tok}
+hed = {'Authorization': 'Bearer ' + CONFIG['WIT_BASE_TOKEN']}
 
 
 def getData(value, expressions) :
-    return {"doc": value+"Doc", "lookups": ["trait"], "id": (value+"UD"), "values": [{"value": value, "expressions": expressions}]}
+    return {"doc": value+"Doc",
+            "lookups": ["trait"],
+            "id": (value+"UD"),
+            "values": [
+                {"value": value, "expressions": expressions}
+            ]}
 
 
 def addEntity(value, epxresstions) :
